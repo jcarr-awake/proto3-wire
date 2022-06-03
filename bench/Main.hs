15f8f851c4467f3fde848a085fab2ad9c42188c1
Jason Carr <jason.carr@arista.com>
2022-06-03 14:57:53 -0700

    Criterion benchmark for "smart" update with delayed parser

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
+
+{-# LANGUAGE UndecidableInstances #-}
 
 
 module Main where
@@ -19,12 +23,40 @@ import Data.Maybe
 import Data.Word
 import Data.IORef
 
-import Criterion (bench)
+import Criterion (bgroup, bench)
 import qualified Criterion as C
 import Criterion.Main (defaultMain)
 
-data Tree a = Leaf | Branch a (Tree a) (Tree a)
-  deriving (Eq, Functor)
+import Control.DeepSeq
+import Data.Functor.Identity
+import Data.Functor.Compose
+import GHC.Exts ( reallyUnsafePtrEquality# )
+import Text.Show.Pretty
+import System.Random
+
+import Debug.Trace
+
+
+data Solo a = Solo a
+  deriving (Functor)
+instance Applicative Solo where
+  pure = Solo
+  Solo f <*> Solo x = Solo (f x)
+
+data Tree f a = Leaf | Branch a (f (Tree f a)) (f (Tree f a))
+  deriving (Functor)
+
+instance (Traversable f, NFData a) => NFData (Tree f a) where
+  rnf Leaf = ()
+  rnf (Branch x t1 t2) =
+    let rnf' x = case rnf x of () -> Solo x
+    in case (rnf x, traverse rnf' t1, traverse rnf' t2) of
+      ((), Solo _, Solo _) -> ()
+
+instance (Functor f, Foldable f) => Foldable (Tree f) where
+  foldMap f Leaf = mempty
+  foldMap f (Branch x t1 t2) =
+    (foldMap.foldMap) f t1 <> f x <> (foldMap.foldMap) f t2
 
 data Rose a = Bud | Rose a [Rose a]
 
@@ -34,19 +66,44 @@ instance Foldable Tree where
 
   sum Leaf = 0
   sum (Branch a t1 t2) =
-    let !a1 = sum t1
-        !a2 = sum t2
+    let !a1 = sum . fmap sum $ t1
+        !a2 = sum . fmap sum $ t2
     in a + a1 + a2
 
 instance Foldable Rose where
   foldMap f Bud = mempty
   foldMap f (Rose x rs) = f x <> ((foldMap.foldMap) f rs)
 
-intTreeParser :: De.Parser De.RawMessage (Tree Word64)
+instance (Eq (f (Tree f a)), Eq a) => Eq (Tree f a) where
+  Leaf == Leaf = True
+  (Branch x1 t11 t12) == (Branch x2 t21 t22) = x1 == x2 && t11 == t21 && t12 == t22
+  _ == _ = False
+
+instance (Show (f (Tree f a)), Show a) => Show (Tree f a) where
+  showsPrec _ Leaf = ('*':)
+  showsPrec _ (Branch x t1 t2) =
+    ('(' :) . shows t1 . ("<-(" ++) . shows x . ("->(" ++) .  shows t2 . (')' :)
+
+type UnparsedTreeRec f a = Either De.ParseError (B.ByteString, Tree f a) -> Either De.ParseError (f (Tree f a))
+
+type Delayed = Compose (Either De.ParseError) ((,) B.ByteString)
+
+
+
+intTreeDelayedParser :: Applicative f => UnparsedTreeRec f Word64 -> De.Parser De.RawMessage (Tree f Word64)
+intTreeDelayedParser f = liftA3 combine
+    (De.at (De.repeated De.fixed64) (FieldNumber 0))
+    (De.unwrapParser $ De.at (De.one (f <$> De.embeddedDelayed' (intTreeDelayedParser f)) (pure $ pure Leaf)) (FieldNumber 1))
+    (De.unwrapParser $ De.at (De.one (f <$> De.embeddedDelayed' (intTreeDelayedParser f)) (pure $ pure Leaf)) (FieldNumber 2))
+  where
+    combine xs y z = Branch (sum xs) y z
+
+
+intTreeParser :: De.Parser De.RawMessage (Tree Identity Word64)
 intTreeParser = liftA3 combine
     (De.at (De.repeated De.fixed64) (FieldNumber 0))
-    (De.at (De.one (De.embedded' intTreeParser) Leaf) (FieldNumber 1))
-    (De.at (De.one (De.embedded' intTreeParser) Leaf) (FieldNumber 2))
+    (Identity <$> De.at (De.one (De.embedded' intTreeParser) Leaf) (FieldNumber 1))
+    (Identity <$> De.at (De.one (De.embedded' intTreeParser) Leaf) (FieldNumber 2))
   where
     combine xs y z = Branch (sum xs) y z
 
@@ -54,6 +111,36 @@ intRoseParser :: De.Parser De.RawMessage (Rose Word64)
 intRoseParser = liftA2 (Rose @Word64)
   (De.at (De.one De.fixed64 0) (FieldNumber 0))
   (De.at (De.repeated (De.embedded' intRoseParser)) (FieldNumber 1))
+
+intTreeEncoder :: Foldable f => Tree f Word64 -> En.MessageBuilder
+intTreeEncoder Leaf = mempty
+intTreeEncoder (Branch w t1 t2) = mconcat [ En.fixed64 (FieldNumber 0) w
+                                          , En.embedded (FieldNumber 1) $ foldMap intTreeEncoder t1
+                                          , En.embedded (FieldNumber 2) $ foldMap intTreeEncoder t2
+                                          ]
+{-# SPECIALIZE intTreeEncoder :: Tree Identity Word64 -> En.MessageBuilder #-}
+{-# SPECIALIZE intTreeEncoder :: Tree Delayed Word64 -> En.MessageBuilder #-}
+
+smartIntTreeEncoder :: Delayed (Tree Delayed Word64) -> Delayed (Tree Delayed Word64) -> En.MessageBuilder
+smartIntTreeEncoder (Compose (Right (bs, t))) (Compose (Right (_, t'))) | 1# <- reallyUnsafePtrEquality# t t' =
+  En.unsafeFromLazyByteString $ BL.fromStrict bs
+smartIntTreeEncoder (Compose (Right (bs, t))) (Compose (Right (_, t'))) =
+    case t' of
+    Leaf -> mempty
+    (Branch w' t1' t2') -> case t of
+      Branch w t1 t2 ->
+        mconcat
+          [ En.fixed64 (FieldNumber 0) w'
+          , En.embedded (FieldNumber 1) $ smartIntTreeEncoder t1 t1'
+          , En.embedded (FieldNumber 2) $ smartIntTreeEncoder t1 t2'
+          ]
+      Leaf ->
+        mconcat
+          [ En.fixed64 (FieldNumber 0) w'
+          , En.embedded (FieldNumber 1) $ foldMap intTreeEncoder t1'
+          , En.embedded (FieldNumber 2) $ foldMap intTreeEncoder t2'
+          ]
+smartIntTreeEncoder _ _ = mempty
 
 detRandom :: [Word64]
 detRandom = concat . replicate 10 $
@@ -128,6 +215,13 @@ pullInt xs = do
       writeIORef xs xs'
       pure x
 
+genInt :: IORef Int -> IO Word64
+genInt count = do
+  count' <- readIORef count
+  if count' <= 0 then pure 7 else do
+    writeIORef count (count' - 1)
+    randomIO
+
 mkTree0 :: IO Word64 -> IO En.MessageBuilder
 mkTree0 ints = do
   shouldFork <- (\(i :: Word64) -> (i `mod` 8) < 6) <$> ints
@@ -159,17 +253,59 @@ mkTree = BL.toStrict . En.toLazyByteString <$> (mkTree0 . pullInt =<< newIORef d
 mkRose :: IO B.ByteString
 mkRose = BL.toStrict . En.toLazyByteString <$> (mkRose0 . pullInt =<< newIORef detRandom)
 
+mkRandomTree :: IO B.ByteString
+mkRandomTree = BL.toStrict . En.toLazyByteString <$> (newIORef 5000 >>= mkTree0 . genInt)
+
 decode :: Foldable f => De.Parser De.RawMessage (f Word64) -> B.ByteString -> IO (Maybe Word64)
 decode p = pure . fmap sum . toMaybe . De.parse p
   where
     toMaybe (Left _) = Nothing
     toMaybe (Right x) = Just x
 
+mkParsed :: Applicative f => UnparsedTreeRec f Word64 -> IO (Tree f Word64)
+mkParsed f = do
+  inp <- mkRandomTree
+  Right t <- pure (De.parse (intTreeDelayedParser f) inp)
+  pure t
+
+mkSmart = mkParsed (Right . Compose)
+mkNaive = mkParsed (fmap (Identity . snd))
+
 unwrap :: (Functor m, Foldable f) => m (f a) -> m a
 unwrap = fmap (foldr1 const)
 
+modifyTree :: Functor f => Tree f Word64 -> Tree f Word64
+modifyTree (Branch x b t3) = Branch x b' t3
+  where b' = fmap switch b
+        switch :: Functor f => Tree f Word64 -> Tree f Word64
+        switch (Branch y t1 t2) | 0 <- y `mod` 5 = Branch (y + 1) (modifyTree <$> t1) (modifyTree <$> t2)
+        switch (Branch y t1 t2) | 1 <- y `mod` 5 = Branch (y + 1) (modifyTree <$> t1) (modifyTree <$> t2)
+        switch (Branch y t1 t2) | 2 <- y `mod` 5 = Branch (y + 1) (modifyTree <$> t1) (modifyTree <$> t2)
+        switch (Branch y t1 t2) | 3 <- y `mod` 5 = Branch (y + 1) t1 (modifyTree <$> t2)
+        switch t = t
+modifyTree t = t
+
+updateSmart :: Tree Delayed Word64 -> IO B.ByteString
+updateNaive :: Tree Delayed Word64 -> IO B.ByteString
+updateSmart t =
+  let t' = modifyTree t
+      encoded = smartIntTreeEncoder (pure t) (pure t')
+  in pure . BL.toStrict . En.toLazyByteString $ encoded
+
+updateNaive t =
+     let t' = modifyTree t in
+     pure . BL.toStrict . En.toLazyByteString . intTreeEncoder $ t'
+
 main :: IO ()
-main =
+main = do
+  let f :: Tree Delayed Word64 -> C.Benchmark
+      f tree = bgroup "Modifies"
+                [ bench "Modify smart" (C.nfIO $ updateSmart tree)
+                , bench "Modify naive" (C.nfIO $ updateNaive tree)
+                ]
   defaultMain
     [ bench "Parse int tree" $ C.perRunEnv mkTree (unwrap . decode intTreeParser)
+    , C.env mkSmart f
     , bench "Parse int rose tree" $ C.perRunEnv mkRose (unwrap . decode intRoseParser)]
+    ]
+
